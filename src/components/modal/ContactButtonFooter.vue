c82151af149686f884461b5558abb63ea8deeb2c
Jaksa Vlahovic <jaksa.vlahovic1@gmail.com>
2025-06-17 20:03:26 +0200

    button in footer fix

---
+++ src/components/modal/ContactButtonFooter.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+import ContactModal from "./ContactModal.vue";
+
+const isOpen = ref(false);
+</script>
+<template>
+  <button
+    @click="isOpen = true"
+    class="bg-festa-500 flex cursor-pointer items-center justify-center px-5 py-2 text-lg font-medium text-white transition-transform hover:-translate-y-1 md:px-8 md:py-4 md:text-2xl"
+  >
+    LET'S TALK
+  </button>
+  <ContactModal v-model:isOpen="isOpen" />
+</template>
